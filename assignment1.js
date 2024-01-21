a25f232203a112cb4644110f69ed4977fb3803d3
SYED UMER SHAH <156168054+sumerfreelance@users.noreply.github.com>
2024-01-21 12:57:21 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+var age = 18;
+var isStudent = true;
+
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+var greeting = "Hello!";
+var introduction = "Welcome to our webpage. We're glad you're here.";
+
+alert(greeting + "\n" + introduction);
---
+++ assignment2.js
@@ -0,0 +1,14 @@
+var length = 10;
+var width = 5;  
+
+var area = length * width;
+
+console.log("Area of the rectangle:", area);
+
+var userName = prompt("Please enter your name:");
+
+if (userName) {
+    alert("Hello, " + userName + "! Welcome to our website.");
+} else {
+    alert("Hello! Welcome to our website.");
+}
